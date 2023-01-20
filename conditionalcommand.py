aa3237b401a454dfa9fa87975d06ca3e9e687e37
Tapu Saha <tapu.saha98@gmail.com>
2023-01-21 04:00:13 +0600

    Update conditionalcommand.py
    Updated

--- conditionalcommand.py
+++ conditionalcommand.py
@@ -5,10 +5,18 @@ import time
 
 driver = webdriver.Chrome(executable_path="C:\Driver\chromedriver_win32\chromedriver.exe")
 driver.maximize_window()
-driver.get("https://www.daraz.com.bd/")
+driver.get("https://www.aiub.edu/")
 time.sleep(2)
 print(driver.title)
 print(driver.current_url)
 driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div/ul/li[1]/a/span").click()
-time.sleep(5)
+time.sleep(2)
+user_ele = driver.find_element(By.NAME, "UserName")
+print(user_ele.is_displayed())
+print(user_ele.is_enabled())
+
+pwd_ele = driver.find_element(By.NAME, "Password")
+print(pwd_ele.is_displayed())
+print(pwd_ele.is_enabled())
+
 
