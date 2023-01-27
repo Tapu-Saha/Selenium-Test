1f43125ab9f5ccd2b305aa8d86939106f452e6d9
Tapu Saha <tapu.saha98@gmail.com>
2023-01-28 00:58:28 +0600

    Update radiobuttoncheck.py

--- radiobuttoncheck.py
+++ radiobuttoncheck.py
@@ -5,12 +5,22 @@ import time
 
 driver = webdriver.Chrome(executable_path="C:\Driver\chromedriver_win32\chromedriver.exe")
 driver.maximize_window()
-driver.get("https://www.alibaba.com/")
+driver.get("https://demo.guru99.com/test/newtours/")
 time.sleep(2)
 print(driver.title)
 print(driver.current_url)
-driver.find_element(By.XPATH, "/html/body/div[1]/header/div[2]/div[4]/div[1]/div[1]/div[2]/a").click()
+ts = driver.find_element(By.NAME,'userName')
+print(ts.is_displayed())
+print(ts.is_enabled())
+ts.send_keys('Tapu')
 time.sleep(2)
-driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/div[2]/div[1]/div/form/div[6]/div[1]/a").click()
+driver.find_element(By.NAME,'password').send_keys('1234')
+time.sleep(2)
+driver.find_element(By.NAME,'submit').click()
+time.sleep(2)
+
+
+
+
 
 
